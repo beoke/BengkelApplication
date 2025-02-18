975456d50679871cb84292f824e226ffe85a37a8
Dhafa Bintang <sucsespart1@gmail.com>
2025-02-18 14:48:02 +0700

    Add project files.

---
+++ BengkelApplication/Models/JasaService.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace BengkelApplication.Models
+{
+    public class JasaService
+    {
+        public int id_jasaServis { get; set; }
+        public string nama_jasaServis { get; set; }
+        public int harga { get; set; }
+    }
+}
