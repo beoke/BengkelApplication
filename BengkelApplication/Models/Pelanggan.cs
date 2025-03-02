eb2e5c6f08a40e61d39d4cbf4983c2077883d113
Dhafa Bintang <sucsespart1@gmail.com>
2025-03-02 12:28:24 +0700

    pelanggan

---
+++ BengkelApplication/Models/Pelanggan.cs
@@ -0,0 +1,13 @@
+﻿namespace BengkelApplication.Models
+{
+    public class Pelanggan
+    {
+        public int No { get; set; }
+        public string ktp_pelanggan { get; set; }
+        public string nama_pelanggan { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string no_telp { get; set; }
+        public string alamat { get; set; }
+    }
+}
